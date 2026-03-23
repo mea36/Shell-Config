3c71aea87af4b0c0cd431141a759b69fa703c96c
Mel Aaron <mel@chromatic.com>
2026-03-23 16:24:20 -0700

    Update readme and setup.bash

--- setup.bash
+++ setup.bash
@@ -1,16 +1,7 @@
 #!/bin/bash
 
-echo "We will need sudo access to install some pyhton packages"
-sudo -v
-
 CONFIGPATH=`pwd`
 echo "Config path: $CONFIGPATH"
-
-echo "Configuring VIM"
-mv ~/.vimrc ~/.vimrc.bak
-ln -sf $CONFIGPATH/vimrc ~/.vimrc 
-mv ~/.vim ~/.vim.bak
-ln -sf $CONFIGPATH/vim ~/.vim
 
 echo "Configuring NVIM"
 mkdir -p ~/.config/nvim
@@ -28,12 +19,6 @@ ln -sf $CONFIGPATH/bash_scripts ~/.bash_scripts
 mv ~/.inputrc ~/.inputrc.bak
 ln -sf $CONFIGPATH/inputrc ~/.inputrc
 
-echo "Configuring ZSH"
-mv ~/.oh-my-zsh ~/.oh-my-zsh.bak
-ln -sf $CONFIGPATH/oh-my-zsh ~/.oh-my-zsh
-mv ~/.zshrc ~/.zshrc.bak
-ln -sf $CONFIGPATH/zshrc ~/.zshrc
-
 echo "Configuring AsciiDoc"
 mv ~/.asciidoc ~/.asciidoc.bak
 ln -sf $CONFIGPATH/asciidoc ~/.asciidoc
@@ -41,15 +26,6 @@ ln -sf $CONFIGPATH/asciidoc ~/.asciidoc
 echo "Configuring SCREEN"
 mv ~/.screenrc ~/.screenrc.bak
 ln -sf $CONFIGPATH/screenrc ~/.screenrc
-
-echo "Installing required python packages"
-sudo pip3 install jedi
-
-echo "Installing VIM plugins"
-git submodule update --init
-
-echo "Upgrading VIM plugins (this may be redundant)"
-git submodule foreach git pull origin master
 
 echo "Install homebrew"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
